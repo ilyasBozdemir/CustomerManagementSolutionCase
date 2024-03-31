adcdf25c04ba2989a7a15caed01f3b846de23730
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-03-31 20:58:54 +0300

    bdd steps added

---
+++ tests/BDD/CustomerManagement.BDD.Tests/StepDefinitions/CustomerManagementSteps.cs
@@ -0,0 +1,66 @@
+﻿using TechTalk.SpecFlow;
+
+namespace CustomerManagement.BDD.Tests.StepDefinitions;
+
+[Binding]
+public class CustomerManagementSteps
+{
+    [Given(@"the user opens the application")]
+    public void GivenTheUserOpensTheApplication()
+    {
+      
+    }
+
+    [When(@"the user selects to create a new customer")]
+    public void WhenTheUserSelectsToCreateANewCustomer()
+    {
+     
+    }
+
+    [When(@"the user enters the name of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersTheNameOfTheNewCustomerAs(string name)
+    {
+     
+    }
+
+    [When(@"the user enters the last name of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersTheLastNameOfTheNewCustomerAs(string lastName)
+    {
+      
+    }
+
+    [When(@"the user enters the date of birth of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersTheDateOfBirthOfTheNewCustomerAs(string dateOfBirth)
+    {
+      
+    }
+
+    [When(@"the user enters the phone number of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersThePhoneNumberOfTheNewCustomerAs(string phoneNumber)
+    {
+      
+    }
+
+    [When(@"the user enters the email address of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersTheEmailAddressOfTheNewCustomerAs(string emailAddress)
+    {
+     
+    }
+
+    [When(@"the user enters the bank account number of the new customer as ""(.*)""")]
+    public void WhenTheUserEntersTheBankAccountNumberOfTheNewCustomerAs(string bankAccountNumber)
+    {
+      
+    }
+
+    [When(@"the user confirms the creation of the new customer")]
+    public void WhenTheUserConfirmsTheCreationOfTheNewCustomer()
+   
+    }
+
+    [Then(@"the user sees that the new customer has been successfully created")]
+    public void ThenTheUserSeesThatTheNewCustomerHasBeenSuccessfullyCreated()
+    {
+       
+    }
+}
